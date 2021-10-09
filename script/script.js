badb82c5df36ae0b16d5dca71ee39fe0bb65c4d6
canozsoy <cnozsy@gmail.com>
2021-10-09 19:59:52 +0300

    rock paper scissors with ui

--- script/script.js
+++ script/script.js
@@ -1,30 +1,31 @@
 let round = 0,
     playerScore = +document.querySelector("#userScore").textContent,
-    computerScore = +document.querySelector("#computerScore").textContent,
-    playAgain = "y";
-
-String.prototype.toSentenceCase = function () {
-    let string = this.valueOf()
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+    computerScore = +document.querySelector("#computerScore").textContent;
 
 const options = ["rock", "paper", "scissors"];
 
 const moveOptions = document.querySelectorAll(".option");
 moveOptions.forEach(x => x.addEventListener("click", playRound));
+const button = document.querySelectorAll("button");
+button.forEach(x => x.addEventListener("click", btnClick));
 
 function playRound(event) {
-    console.log(event.target.nodeType)
-    const userMove = event.target.getAttribute("data-value");
+    let target = event.target.tagName === "IMG" ? event.target.offsetParent
+        : event.target;
     
+    const userMove = target.getAttribute("data-value");
     const computerMove = computerPlay();
-    console.log(userMove, computerMove)
+    const userNode = document.querySelector("#userMove");
+    const computerNode = document.querySelector("#computerMove");
+    prepareImg("./static/" + userMove + ".svg", userMove, userNode);
+    prepareImg("./static/" + computerMove + ".svg", computerMove, computerNode);
     const message = compareScore(userMove, computerMove);
+    document.querySelector("#scoreHeader").textContent = message;
+    document.querySelector("#userScore").textContent = playerScore;
+    document.querySelector("#computerScore").textContent = computerScore;
     round++;
-    document.querySelector("#roundScore").textContent = `Round: ${round}`;
-    document.querySelector("#roundWinner").textContent = message;
-    if (round === 6) {
-        moveOptions.forEach(x => x.removeEventListener("click", playRound));
+    
+    if (round === 5) {
         finishTheGame();
     }
 }
@@ -38,6 +39,8 @@ function compareScore(playerSelection, computerSelection) {
     let message;
     if (playerSelection === computerSelection) {
         message = "It's a Tie!";
+        computerScore++;
+        playerScore++;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         message = "Computer Wins!";
         computerScore++;
@@ -54,6 +57,43 @@ function compareScore(playerSelection, computerSelection) {
     return message;
 }
 
+function prepareImg (src, alt, parent) {
+    parent.innerHTML = "";
+    const img = document.createElement("img");
+    img.src = src;
+    img.alt = alt;
+    parent.appendChild(img);
+}
+
 function finishTheGame() {
-    console.log("Finish")
+    moveOptions.forEach(x => x.removeEventListener("click", playRound));
+    prepareScoreTable();
+}
+
+function prepareScoreTable() {
+    let text = playerScore > computerScore ?
+                    "You Won!"
+                    : computerScore > playerScore ?
+                    "You Lost!"
+                    : "It's a Tie!";
+                    text += " Wanna Play Again?";
+    const targetNode = document.querySelector(".scoreTableText");
+    targetNode.textContent = text;
+    document.querySelector(".scoreTableContainer").style.display = "flex";
+}
+
+function closeScoreTable() {
+    document.querySelector(".scoreTableContainer").style.display = "none";
+}
+
+function initialize () {
+    window.location.reload();
+}
+
+function btnClick (event) {
+    if (event.target.getAttribute("data-play") === "y") {
+        initialize();
+    } else {
+        closeScoreTable();
+    }
 }
